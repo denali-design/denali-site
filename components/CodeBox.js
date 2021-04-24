8690c520ba2a7a7f4adfa36ffefe13e126ae2f86
Chas Turansky <chas@turansky.net>
2021-04-23 19:47:57 -0700

    Updated alert page

--- components/CodeBox.js
+++ components/CodeBox.js
@@ -3,16 +3,29 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import codeTheme from 'prism-react-renderer/themes/okaidia';
 
-export default function CodeBox({ props, children }) {
+export default function CodeBox({ source }) {
     return (
         <div className="m-t-30">
-            {/* <div className="has-bg-grey-200">
-                {children}
-            </div> */}
-
-            <Highlight {...defaultProps} code={children} language="html" theme={codeTheme}>
+            <div className="has-bg-grey-200 br-lg overflow-hidden m-b-20">
+                <iframe style={{width: '100%'}} srcDoc={`
+                    <head>
+                        <style>
+                            body {
+                                background: transparent !important;
+                            }
+                        </style>
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/denali-css/css/denali.css">
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/denali-icon-font/dist/denali-icon-font.css">
+                    </head>
+                    <body class='p-20'>
+                        ${source}
+                    </body>
+                `} />
+            </div>
+                            
+            <Highlight {...defaultProps} code={source} language="html" theme={codeTheme}>
                 {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                    <pre className={className} style={{ ...style, padding: '20px' }}>
+                    <pre className={className} style={{ ...style, padding: '20px', overflow: 'scroll' }}>
                         {tokens.map((line, i) => (
                             <div key={i} {...getLineProps({ line, key: i })}>
                                 {line.map((token, key) => (
